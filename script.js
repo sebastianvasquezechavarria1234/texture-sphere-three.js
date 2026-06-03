bd0087b61a176e303e3b79772e7e9d00ce3e94a3
sebastianvasquezechavarria1234 <sebasvasquez1314@gmail.com>
2026-06-03 09:54:35 -0500

    refactor: implement placeholder textures to prevent loading artifacts

--- script.js
+++ script.js
@@ -25,35 +25,70 @@ controls.maxDistance = 48;
 const textureLoader = new THREE.TextureLoader();
 const exrLoader = new EXRLoader();
 
-const bgTexture = textureLoader.load('./img/scene-bg.jpg');
+function createPlaceholder(r, g, b) {
+  const canvas = document.createElement('canvas');
+  canvas.width = 4;
+  canvas.height = 4;
+  const ctx = canvas.getContext('2d');
+  ctx.fillStyle = `rgb(${r},${g},${b})`;
+  ctx.fillRect(0, 0, 4, 4);
+  return new THREE.CanvasTexture(canvas);
+}
+
+const bgPlaceholder = createPlaceholder(30, 30, 50);
+const diffusePlaceholder = createPlaceholder(128, 128, 128);
+const normalPlaceholder = createPlaceholder(128, 128, 255);
+const roughnessPlaceholder = createPlaceholder(200, 200, 200);
+const displacementPlaceholder = createPlaceholder(128, 128, 128);
+
 const bgGeometry = new THREE.SphereGeometry(50, 64, 64);
 const bgMaterial = new THREE.MeshBasicMaterial({
-  map: bgTexture,
+  map: bgPlaceholder,
   side: THREE.BackSide
 });
 const bgSphere = new THREE.Mesh(bgGeometry, bgMaterial);
 scene.add(bgSphere);
 
-const diffuseTexture = textureLoader.load('./img/texture/textures/marble_cliff_03_diff_4k.jpg');
-const normalTexture = exrLoader.load('./img/texture/textures/marble_cliff_03_nor_gl_4k.exr');
-const roughnessTexture = exrLoader.load('./img/texture/textures/marble_cliff_03_rough_4k.exr');
-const displacementTexture = textureLoader.load('./img/texture/textures/marble_cliff_03_disp_4k.png');
-
 const geometry = new THREE.SphereGeometry(2, 128, 128);
 
 const material = new THREE.MeshStandardMaterial({
-  map: diffuseTexture,
-  normalMap: normalTexture,
+  map: diffusePlaceholder,
+  normalMap: normalPlaceholder,
   normalScale: new THREE.Vector2(1.5, 1.5),
-  roughnessMap: roughnessTexture,
+  roughnessMap: roughnessPlaceholder,
   roughness: 1.0,
-  displacementMap: displacementTexture,
+  displacementMap: displacementPlaceholder,
   displacementScale: 0.3,
   displacementBias: -0.1,
 });
 
 const sphere = new THREE.Mesh(geometry, material);
 scene.add(sphere);
+
+textureLoader.load('./img/scene-bg.jpg', (texture) => {
+  bgMaterial.map = texture;
+  bgMaterial.needsUpdate = true;
+});
+
+textureLoader.load('./img/texture/textures/marble_cliff_03_diff_4k.jpg', (texture) => {
+  material.map = texture;
+  material.needsUpdate = true;
+});
+
+exrLoader.load('./img/texture/textures/marble_cliff_03_nor_gl_4k.exr', (texture) => {
+  material.normalMap = texture;
+  material.needsUpdate = true;
+});
+
+exrLoader.load('./img/texture/textures/marble_cliff_03_rough_4k.exr', (texture) => {
+  material.roughnessMap = texture;
+  material.needsUpdate = true;
+});
+
+textureLoader.load('./img/texture/textures/marble_cliff_03_disp_4k.png', (texture) => {
+  material.displacementMap = texture;
+  material.needsUpdate = true;
+});
 
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.4);
 scene.add(ambientLight);
